1128a0b6624e7d70eb4e8794431aff3fdf5017c3
Gauri Agrawal <118062523+GauriGA@users.noreply.github.com>
2023-12-27 21:31:25 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function toggleSidebar() {
-    document.querySelector('.sidebar').classList.toggle('collapsed');
-  }
-  
-  function logout() {
-    // Add your logout logic here
-    alert("Logout clicked");
-  }
