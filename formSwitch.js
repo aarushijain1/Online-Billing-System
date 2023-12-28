7bff8bc5d8222e7ea334598bb3f6f65fb6a1362d
Gauri Agrawal <118062523+GauriGA@users.noreply.github.com>
2023-12-28 12:37:16 +0530

    Add files via upload

---
+++ formSwitch.js
@@ -0,0 +1,7 @@
+// function showRegistrationForm() {
+//     window.location.href = 'registration_form.php';
+// }
+
+document.getElementById('c').addEventListener('click', function () {
+    window.open('registration_form.php','_blank');
+});
