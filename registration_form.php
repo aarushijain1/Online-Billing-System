cc6c5fbb61284ae73abc043c8d99b4ef4c422664
Gauri Agrawal <118062523+GauriGA@users.noreply.github.com>
2023-12-29 09:22:25 +0530

    Update registration_form.php

--- registration_form.php
+++ registration_form.php
@@ -1,41 +1,3 @@
-<?php
-session_start();
-
-if ($_POST) {
-    include("connection.php");
-
-    $first_name = $_POST['first_name'];
-    $last_name = $_POST['last_name'];
-    $email = $_POST['email'];
-    $password = $_POST['password'];
-
-    // Validate and sanitize user input to prevent SQL injection
-    $email = filter_var($email, FILTER_SANITIZE_EMAIL);
-    $password = filter_var($password, FILTER_SANITIZE_STRING);
-
-    // Hash the password for security
-    $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
-
-    // Insert user data into the database
-    $query = 'INSERT INTO erp_portal.users (first_name, last_name, email, password) VALUES (?, ?, ?, ?)';
-    $stmt = $conn->prepare($query);
-    $stmt->execute([$first_name, $last_name, $email, $hashedPassword]);
-
-    // Fetch the user information from the database after registration
-    $query = 'SELECT * FROM users WHERE email = ?';
-    $stmt = $conn->prepare($query);
-    $stmt->execute([$email]);
-
-    if ($stmt->rowCount() > 0) {
-        $stmt->setFetchMode(PDO::FETCH_ASSOC);
-        $user = $stmt->fetchAll()[0];
-        $_SESSION['user'] = $user;
-    }
-
-    header('Location: username.php');
-}
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -48,25 +10,28 @@ if ($_POST) {
 
 <body>
 
-    <form id = "registration_form" action="#" method="POST">
+    <form id = "registration_form" action="reg.php" method="POST">
 
-    <div class="container">
+        <div class="container">
             <img src="logo.jpg" alt="IGDTUW LOGO" height="100px">
             <div class="text">
                 <p>INDIRA GANDHI DELHI TECHNICAL UNIVERSITY FOR WOMEN</p>
                 <p>Kashmere Gate, Delhi-110006</p>
+                <p>REGISTRATION FORM</p>
             </div>
-    </div>
-    
-        <h2>Registration Form</h2>
-
+        </div>
+        <br>
         <!-- Personal Details -->
+        <h3>Faculty Profile</h3>
+
         <div class="form-group">
             <label for="first_name">First Name:</label>
             <input type="text" id="first_name" name="first_name" required>
+        </div>
 
+        <div class="form-group">
             <label for="last_name">Last Name:</label>
-            <input type="text" id="last_name" name="last_name" required>
+            <input type="text" id="last_name" name="last_name">
         </div>
 
         <div class="form-group">
@@ -83,32 +48,81 @@ if ($_POST) {
             </select>
         </div>
 
+        <!-- Contact Details -->
+        <h3>Contact Details</h3>
+
         <div class="form-group">
-            <label for="phoneNumber">Phone Number:</label>
-            <input type="tel" id="phoneNumber" name="phoneNumber" required>
+            <label for="resphoneNumber">Residential Telephone Number:</label>
+            <input type="tel" id="resphoneNumber" name="resphoneNumber">
+        </div>
+
+        <div class="form-group">
+            <label for="offphoneNumber">Office Telephone Number:</label>
+            <input type="tel" id="offphoneNumber" name="offphoneNumber" required>
+        </div>
+
+        <div class="form-group">
+            <label for="mobileNumber">Mobile Number:</label>
+            <input type="tel" id="mobileNumber" name="mobileNumber" required>
         </div>
 
         <div class="form-group">
             <label for="email">Email:</label>
-            <input type="email" id="email" name="email" required>
+            <input type="email" id="email" name="email">
         </div>
 
+        <div class="form-group">
+            <label for="offemail">Official Email:</label>
+            <input type="email" id="offemail" name="offemail" required>
+        </div>
+        <!-- Financial Details -->
+        <h3>Financial Details</h3>
+
+        <div class="form-group">
+            <label for="accno">Account Number:</label>
+            <input type="number" id="accno" name="accno" required>
+        </div>
+
+        <div class="form-group">
+            <label for="accname">Acount Holder Name:</label>
+            <input type="text" id="accname" name="accname" required>
+        </div>
+
+        <div class="form-group">
+            <label for="bank">Bank Name:</label>
+            <input type="text" id="bank" name="bank" required>
+        </div>
+
+        <div class="form-group">
+            <label for="ifsc">IFSC Code:</label>
+            <input type="text" id="ifsc" name="ifsc" required>
+        </div>
+
+        <div class="form-group">
+            <label for="pan">PAN Number:</label>
+            <input type="number" id="pan" name="pan" required>
+        </div>
         <!-- Address Details -->
         <h3>Address Details</h3>
 
         <div class="form-group">
-            <label for="addressType">Address Type:</label>
-            <input type="text" id="addressType" name="addressType">
+            <label for="address">Address:</label>
+            <input type="text" id="address" name="address">
         </div>
 
         <div class="form-group">
-            <label for="nationality">Nationality:</label>
-            <input type="text" id="nationality" name="nationality">
+            <label for="country">Country:</label>
+            <input type="text" id="country" name="country" required>
         </div>
 
         <div class="form-group">
             <label for="state">State:</label>
-            <input type="text" id="state" name="state">
+            <input type="text" id="state" name="state" required>
+        </div>
+
+        <div class="form-group">
+            <label for="state">City:</label>
+            <input type="text" id="city" name="city">
         </div>
 
         <div class="form-group">
@@ -117,21 +131,16 @@ if ($_POST) {
         </div>
 
         <div class="form-group">
-            <label for="addressLine1">Address Line 1:</label>
-            <input type="text" id="addressLine1" name="addressLine1">
+            <label for="pincode">Pin Code:</label>
+            <input type="number" id="pincode" name="pincode" required>
         </div>
-
-        <div class="form-group">
-            <label for="addressLine2">Address Line 2:</label>
-            <input type="text" id="addressLine2" name="addressLine2">
-        </div>
-
         <!-- Professional Details -->
         <h3>Professional Details</h3>
 
         <div class="form-group">
             <label for="position">Position:</label>
             <select id="position" name="position">
+                <option value="" disabled selected>Select an option</option>
                 <option value="professor">Professor</option>
                 <option value="assistantProfessor">Assistant Professor</option>
                 <option value="associateProfessor">Associate Professor</option>
@@ -142,6 +151,7 @@ if ($_POST) {
         <div class="form-group">
             <label for="department">Department:</label>
             <select id="department" name="department">
+                <option value="" disabled selected>Select an option</option>
                 <option value="appliedSciences">Applied Sciences and Humanities</option>
                 <option value="computerScience">Computer Science and Engineering</option>
                 <option value="electronicsCommunication">Electronics and Communication Engineering</option>
@@ -161,6 +171,7 @@ if ($_POST) {
         <div class="form-group">
             <label for="employmentStatus">Employment Status:</label>
             <select id="employmentStatus" name="employmentStatus">
+                <option value="" disabled selected>Select an option</option>
                 <option value="visiting">Visiting</option>
                 <option value="fullTime">Full-Time</option>
                 <option value="partTime">Part-Time</option>
@@ -176,23 +187,22 @@ if ($_POST) {
         <h3>Upload Documents</h3>
 
         <div class="form-group">
-            <label for="aadhar">Aadhar:</label>
-            <input type="file" id="aadhar" name="aadhar" accept=".jpg, .jpeg, .png" required>
+            <label for="image">Passport Image:</label>
+            <input type="file" id="image" name="image" accept=".jpg, .jpeg, .png, .pdf" required>
         </div>
-
         <div class="form-group">
-            <label for="pan">PAN:</label>
-            <input type="file" id="pan" name="pan" accept=".jpg, .jpeg, .png" required>
+            <label for="pancard">PAN Card:</label>
+            <input type="file" id="pancard" name="pancard" accept=".jpg, .jpeg, .png, .pdf" required>
         </div>
-
         <div class="form-group">
             <label for="resume">Resume:</label>
-            <input type="file" id="resume" name="resume" accept=".pdf" required>
+            <input type="file" id="resume" name="resume" accept=".jpg, .jpeg, .png, .pdf" required>
         </div>
 
         <!-- Submit Button -->
         <button type="submit">Submit</button>
         <button type="reset">Reset</button>
+
     </form>
 
 </body>
