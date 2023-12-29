922c381888ccba5839c55e5d0858ae1a568b7514
Gauri Agrawal <118062523+GauriGA@users.noreply.github.com>
2023-12-29 09:14:59 +0530

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -29,11 +29,6 @@
         <!-- sidebar -->
 
         <div class="main-content">
-            <div class="top-bar">
-                <div class="toggle-btn" onclick="toggleSidebar()">&#9776;</div>
-                <h2>Dashboard</h2>
-                <a href = "Logout.php" div class="logout-btn" onclick="logout()"><i class="fas fa-sign-out-alt"></i></div>
-            </div>
             <!-- Rest of your main content goes here -->
         </div>
     </div>
