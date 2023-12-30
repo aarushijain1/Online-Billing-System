e3555f39885465413bb90fd344502a5910377023
Gauri Agrawal <118062523+GauriGA@users.noreply.github.com>
2023-12-30 10:36:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,21 @@ function toggleSidebar() {
     // Add your logout logic here
     alert("Logout clicked");
   }
+
+  document.addEventListener('DOMContentLoaded', function () {
+    // Get all menu items
+    var menuItems = document.querySelectorAll('.menu-item');
+
+    // Add a click event listener to each menu item
+    menuItems.forEach(function (menuItem) {
+        menuItem.addEventListener('click', function () {
+            // Remove "active" class from all menu items
+            menuItems.forEach(function (item) {
+                item.classList.remove('active');
+            });
+
+            // Add "active" class to the clicked menu item
+            menuItem.classList.add('active');
+        });
+    });
+});
