4b0421f0d2272cad845ea617a7b264fc7e67f8bc
Gauri Agrawal <118062523+GauriGA@users.noreply.github.com>
2024-01-09 23:54:24 +0530

    Update ppreval.php

--- ppreval.php
+++ ppreval.php
@@ -4,6 +4,50 @@
     if(!isset($_SESSION['user'])) header('location: dashboard.php');
 
     $user = $_SESSION['user'];
+
+    include("connection.php");
+    // Fetch PAN card information from the newreg table
+    $panQuery = 'SELECT * FROM erp_portal.newreg WHERE first_name = ?';
+    $panStmt = $conn->prepare($panQuery);
+    $panStmt->execute([$user['first_name']]);
+    $panInfo = $panStmt->fetch(PDO::FETCH_ASSOC);
+
+    // Fetch bank no information from the newreg table
+    $bankQuery = 'SELECT * FROM erp_portal.newreg WHERE first_name = ?';
+    $bankStmt = $conn->prepare($bankQuery);
+    $bankStmt->execute([$user['first_name']]);
+    $bankInfo = $bankStmt->fetch(PDO::FETCH_ASSOC);
+
+    // Fetch ifsc information from the newreg table
+    $ifscQuery = 'SELECT * FROM erp_portal.newreg WHERE first_name = ?';
+    $ifscStmt = $conn->prepare($ifscQuery);
+    $ifscStmt->execute([$user['first_name']]);
+    $ifscInfo = $ifscStmt->fetch(PDO::FETCH_ASSOC);
+
+    // Fetch res_add information from the newreg table
+    $addressQuery = 'SELECT * FROM erp_portal.newreg WHERE first_name = ?';
+    $addressStmt = $conn->prepare($addressQuery);
+    $addressStmt->execute([$user['first_name']]);
+    $addressInfo = $addressStmt->fetch(PDO::FETCH_ASSOC);
+
+    // Fetch off_add information from the newreg table
+    $off_addressQuery = 'SELECT * FROM erp_portal.newreg WHERE first_name = ?';
+    $off_addressStmt = $conn->prepare($off_addressQuery);
+    $off_addressStmt->execute([$user['first_name']]);
+    $off_addressInfo = $off_addressStmt->fetch(PDO::FETCH_ASSOC);
+
+    // Fetch mob_no information from the newreg table
+    $mobileNumberQuery = 'SELECT * FROM erp_portal.newreg WHERE first_name = ?';
+    $mobileNumberStmt = $conn->prepare($mobileNumberQuery);
+    $mobileNumberStmt->execute([$user['first_name']]);
+    $mobileNumberInfo = $mobileNumberStmt->fetch(PDO::FETCH_ASSOC);
+
+    // Fetch email information from the newreg table
+    $emailQuery = 'SELECT * FROM erp_portal.newreg WHERE first_name = ?';
+    $emailStmt = $conn->prepare($emailQuery);
+    $emailStmt->execute([$user['first_name']]);
+    $emailInfo = $emailStmt->fetch(PDO::FETCH_ASSOC);
+
 ?>
 
 <!DOCTYPE html>
@@ -37,14 +81,14 @@
             <input type="text" id="formNumber" name="formNumber" readonly>
         </div> -->
 
-        <p>1. Name of the Paper Setter: <span><?= $_SESSION['user']['first_name'] . ' ' . $_SESSION['user']['last_name'] ?></span> </p>
-        <p>2. PAN No.: </p>
-        <p>3. Bank Account No. : </p>
-        <p>4. IFSC Code: </p>
-        <p>5. Residential Address: </p>
-        <p>6. Office Address: </p>
-        <p>7. Mobile No. : </p>
-        <p>8. Email Id: </p>
+        <p>1. Name of the Paper Evaluator: <span><?= $_SESSION['user']['first_name'] . ' ' . $_SESSION['user']['last_name'] ?></span></p>
+        <p>2. PAN No.: <?= isset($panInfo['pan']) ? $panInfo['pan'] : 'Not available' ?></p>
+        <p>3. Bank Account No. : <?= isset($bankInfo['bank_no']) ? $bankInfo['bank_no'] : 'Not available' ?></p>
+        <p>4. IFSC Code: <?= isset($ifscInfo['ifsc']) ? $ifscInfo['ifsc'] : 'Not available' ?></p> </p>
+        <p>5. Residential Address: <?= isset($addressInfo['address']) ? $addressInfo['address'] : 'Not available' ?></p> </p>
+        <p>6. Office Address: <?= isset($off_addressInfo['off_address']) ? $off_addressInfo['off_address'] : 'Not available' ?></p> </p>
+        <p>7. Mobile No. : <?= isset($mobileNumberInfo['mobileNumber']) ? $mobileNumberInfo['mobileNumber'] : 'Not available' ?></p> </p>
+        <p>8. Email Id: <?= isset($emailInfo['email']) ? $emailInfo['email'] : 'Not available' ?></p> </p>
         <p>9. Evaluation Details</p>
         <p> Have you set the Q.P. of the Subject</p>
         <label>
