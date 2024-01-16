058cf8fc6873281477ac6be386335ba1fc465676
Gauri Agrawal <118062523+GauriGA@users.noreply.github.com>
2024-01-16 08:57:37 +0530

    Update script3.js

--- script3.js
+++ script3.js
@@ -8,3 +8,23 @@ const currentDate = new Date();
 
 const currentDateElement = document.getElementById("currentDate");
 currentDateElement.textContent = "Dated: " + formatDate(currentDate);
+
+function showSubjectCodeMessage() {
+    var message = document.getElementById("subjectCodeMessage");
+    message.style.display = "block";
+}
+
+function hideSubjectCodeMessage() {
+    var message = document.getElementById("subjectCodeMessage");
+    message.style.display = "none";
+}
+
+function showSubjectMessage() {
+    var message = document.getElementById("subjectMessage");
+    message.style.display = "block";
+}
+
+function hideSubjectMessage() {
+    var message = document.getElementById("subjectMessage");
+    message.style.display = "none";
+}
