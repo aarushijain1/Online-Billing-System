4c127a1485803578f7af5903dc71bece31588ad9
Gauri Agrawal <118062523+GauriGA@users.noreply.github.com>
2024-01-20 11:23:42 +0530

    Add files via upload

---
+++ prac.js
@@ -0,0 +1,29 @@
+function showSubjectCodeMessage() {
+    var message = document.getElementById("subjectCodeMessage");
+    message.style.display = "block";
+}
+
+function hideSubjectCodeMessage() {
+    var message = document.getElementById("subjectCodeMessage");
+    message.style.display = "none";
+}
+
+function showSubjectMessage() {
+    var message = document.getElementById("subjectMessage");
+    message.style.display = "block";
+}
+
+function hideSubjectMessage() {
+    var message = document.getElementById("subjectMessage");
+    message.style.display = "none";
+}
+
+function showYearMessage() {
+    var message = document.getElementById("yearMessage");
+    message.style.display = "block";
+}
+
+function hideYearMessage() {
+    var message = document.getElementById("yearMessage");
+    message.style.display = "none";
+}
