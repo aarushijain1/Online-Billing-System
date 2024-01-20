4b8ba6158bd34756c4a0741398645544a55e49d2
Gauri Agrawal <118062523+GauriGA@users.noreply.github.com>
2024-01-20 11:19:54 +0530

    Update formSwitch.js

--- formSwitch.js
+++ formSwitch.js
@@ -1,7 +1,3 @@
-// function showRegistrationForm() {
-//     window.location.href = 'registration_form.php';
-// }
-
 document.getElementById('c').addEventListener('click', function () {
     window.open('registration_form.php','_blank');
 });
