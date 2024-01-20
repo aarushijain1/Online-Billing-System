bca41d78c9432a9b400d56f80c31cba348e0bce5
Gauri Agrawal <118062523+GauriGA@users.noreply.github.com>
2024-01-20 11:25:10 +0530

    Update script1.js

--- script1.js
+++ script1.js
@@ -1,21 +1,3 @@
-//Form Number
-// document.addEventListener('DOMContentLoaded', function () {
-
-//     var form = document.getElementById('pprset');
-//     var formNumberInput = document.getElementById('formNumber');
-
-//     var formNumber = 1;
-//     formNumberInput.value = formNumber;
-
-//     form.addEventListener('submit', function (event) {
-//         event.preventDefault();
-
-//         formNumber++;
-//         formNumberInput.value = formNumber;
-
-//     });
-// });
-
 // Current Date
     function formatDate(date) {
         const options = { year: 'numeric', month: 'long', day: 'numeric' };
