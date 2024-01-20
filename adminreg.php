712dabbd4e80b5291f5e8b6af94b98840bd71587
Gauri Agrawal <118062523+GauriGA@users.noreply.github.com>
2024-01-20 11:44:23 +0530

    Update adminreg.php

--- adminreg.php
+++ adminreg.php
@@ -21,28 +21,26 @@ if ($_POST) {
     // Validate and sanitize user input to prevent SQL injection
     $email = filter_var($email, FILTER_SANITIZE_EMAIL);
 
-    // Insert user data into the database
-    // Insert user data into the database
     $query = 'INSERT INTO erp_portal.adminnewreg (
         first_name, last_name, dob, gender, mobileNumber, offemail, address, country, state, city, district, pincode, image) 
         VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
-    $stmt = $conn->prepare($query);
-    $stmt->execute([
+        $stmt = $conn->prepare($query);
+        $stmt->execute([
         $first_name, $last_name, $dob, $gender, $mobileNumber, $offemail, $address, $country, $state, $city, $district, $pincode, 
         $image]);
     
 
     // Fetch the user information from the database after registration
-    $query = 'SELECT * FROM users WHERE email = ?';
-    $stmt = $conn->prepare($query);
-    $stmt->execute([$email]);
+    // $query = 'SELECT * FROM users WHERE email = ?';
+    // $stmt = $conn->prepare($query);
+    // $stmt->execute([$email]);
 
-    if ($stmt->rowCount() > 0) {
-        $stmt->setFetchMode(PDO::FETCH_ASSOC);
-        $user = $stmt->fetchAll()[0];
-        $_SESSION['user'] = $user;
-    }
+    // if ($stmt->rowCount() > 0) {
+    //     $stmt->setFetchMode(PDO::FETCH_ASSOC);
+    //     $user = $stmt->fetchAll()[0];
+    //     $_SESSION['user'] = $user;
+    // }
 
-    header('Location: username.php');
+    header('Location: adminusername.php');
 }
 ?>
