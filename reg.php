d1a6b157180c20d17113cbdf142f05caa64463eb
Gauri Agrawal <118062523+GauriGA@users.noreply.github.com>
2024-01-22 23:10:27 +0530

    Update reg.php

--- reg.php
+++ reg.php
@@ -1,72 +1,67 @@
 <?php
 session_start();
 
-if ($_POST) {
-    include("connection.php");
+if ( $_POST ) {
+    include( 'connection.php' );
 
-    $first_name = $_POST['first_name'];
-    $last_name = $_POST['last_name'];
-    $dob = $_POST['dob'];
-    $gender = $_POST['gender'];
-    $resphoneNumber = $_POST['resphoneNumber'];
-    $offphoneNumber = $_POST['offphoneNumber'];
-    $mobileNumber = $_POST['mobileNumber'];
-    $email = $_POST['email'];
-    $offemail = $_POST['offemail'];
-    $accno = $_POST['accno'];
-    $accname = $_POST['accname'];
-    $bank = $_POST['bank'];
+    $first_name = $_POST[ 'first_name' ];
+    $last_name = $_POST[ 'last_name' ];
+    $dob = $_POST[ 'dob' ];
+    $gender = $_POST[ 'gender' ];
+    $resphoneNumber = $_POST[ 'resphoneNumber' ];
+    $offphoneNumber = $_POST[ 'offphoneNumber' ];
+    $mobileNumber = $_POST[ 'mobileNumber' ];
+    $email = $_POST[ 'email' ];
+    $offemail = $_POST[ 'offemail' ];
+    $accno = $_POST[ 'accno' ];
+    $accname = $_POST[ 'accname' ];
+    $bank = $_POST[ 'bank' ];
+    $otherBankName = $_POST[ 'otherBank' ];
+    $bankName = ( $selectedBank === 'other' ) ? $otherBankName : $selectedBank;
+    $ifsc = $_POST[ 'ifsc' ];
+    $pan = $_POST[ 'pan' ];
+    $address = $_POST[ 'address' ];
+    $country = $_POST[ 'country' ];
+    $state = $_POST[ 'state' ];
+    $city = $_POST[ 'city' ];
+    $district = $_POST[ 'district' ];
+    $pincode = $_POST[ 'pincode' ];
+    $position = $_POST[ 'position' ];
+    $department = $_POST[ 'department' ];
+    $institution = $_POST[ 'institution' ];
+    $employmentStatus = $_POST[ 'employmentStatus' ];
+    $image = $_POST[ 'image' ];
+    $pancard = $_POST[ 'pancard' ];
+    $resume = $_POST[ 'resume' ];
+    $off_address = $_POST[ 'off_address' ];
+    $sign = $_POST[ 'sign' ];
+    $bank_branch = $_POST[ 'bank_branch' ];
 
-    $otherBankName = $_POST['otherBank'];
-    $bankName = ($selectedBank === 'other') ? $otherBankName : $selectedBank;
+    $email = filter_var( $email, FILTER_SANITIZE_EMAIL );
 
-    $ifsc = $_POST['ifsc'];
-    $pan = $_POST['pan'];
-    $address = $_POST['address'];
-    $country = $_POST['country'];
-    $state = $_POST['state'];
-    $city = $_POST['city'];
-    $district = $_POST['district'];
-    $pincode = $_POST['pincode'];
-    $position = $_POST['position'];
-    $department = $_POST['department'];
-    $institution = $_POST['institution'];
-    $employmentStatus = $_POST['employmentStatus'];
-    $image = $_POST['image'];
-    $pancard = $_POST['pancard'];
-    $resume = $_POST['resume'];
-    $off_address = $_POST['off_address'];
-    $sign = $_POST['sign'];
-    $bank_branch = $_POST['bank_branch'];
-
-    // Validate and sanitize user input to prevent SQL injection
-    $email = filter_var($email, FILTER_SANITIZE_EMAIL);
-
-    // Insert user data into the database
-    // Insert user data into the database
     $query = 'INSERT INTO erp_portal.newreg (
         first_name, last_name, dob, gender, resphoneNumber, offphoneNumber, mobileNumber, email, offemail, accno, accname, bank,otherBankName, ifsc, pan, address, country, state, city, district, pincode, position, department, institution, employmentStatus, image, 
         pancard, resume, off_address, sign, bank_branch) 
         VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?,?, ?, ?)';
-    $stmt = $conn->prepare($query);
-    $stmt->execute([
-        $first_name, $last_name, $dob, $gender, $resphoneNumber, $offphoneNumber, $mobileNumber, $email, $offemail, 
-        $accno, $accname, $bank,$otherBankName, $ifsc, $pan, $address, $country, $state, $city, $district, $pincode, 
+    $stmt = $conn->prepare( $query );
+    $stmt->execute( [
+        $first_name, $last_name, $dob, $gender, $resphoneNumber, $offphoneNumber, $mobileNumber, $email, $offemail,
+        $accno, $accname, $bank, $otherBankName, $ifsc, $pan, $address, $country, $state, $city, $district, $pincode,
         $position, $department, $institution, $employmentStatus, $image, $pancard, $resume, $off_address, $sign, $bank_branch
-    ]);
-    
+    ] );
 
     // Fetch the user information from the database after registration
     $query = 'SELECT * FROM users WHERE email = ?';
-    $stmt = $conn->prepare($query);
-    $stmt->execute([$email]);
+    $stmt = $conn->prepare( $query );
+    $stmt->execute( [ $email ] );
 
-    if ($stmt->rowCount() > 0) {
-        $stmt->setFetchMode(PDO::FETCH_ASSOC);
-        $user = $stmt->fetchAll()[0];
-        $_SESSION['user'] = $user;
+    if ( $stmt->rowCount() > 0 ) {
+        $stmt->setFetchMode( PDO::FETCH_ASSOC );
+        $user = $stmt->fetchAll()[ 0 ];
+        $_SESSION[ 'user' ] = $user;
     }
 
-    header('Location: username.php');
+    header( 'Location: username.php' );
 }
 ?>
+
