21533c17ec77fe5734af28d4c1c1ed1c5a9d1bfe
Gauri Agrawal <118062523+GauriGA@users.noreply.github.com>
2024-01-22 23:11:34 +0530

    Update script1.js

--- script1.js
+++ script1.js
@@ -1,30 +1,39 @@
-// Current Date
-    function formatDate(date) {
-        const options = { year: 'numeric', month: 'long', day: 'numeric' };
-        return date.toLocaleDateString(undefined, options);
-    }
+function formatDate(date) {
+  const options = { year: "numeric", month: "long", day: "numeric" };
+  return date.toLocaleDateString(undefined, options);
+}
 
-    const currentDate = new Date();
+const currentDate = new Date();
 
-    const currentDateElement = document.getElementById("currentDate");
-    currentDateElement.textContent = "Dated: " + formatDate(currentDate);
+const currentDateElement = document.getElementById("currentDate");
+currentDateElement.textContent = "Dated: " + formatDate(currentDate);
 
-    function showSubjectCodeMessage() {
-        var message = document.getElementById("subjectCodeMessage");
-        message.style.display = "block";
-    }
+function showSubjectCodeMessage() {
+  var message = document.getElementById("subjectCodeMessage");
+  message.style.display = "block";
+}
 
-    function hideSubjectCodeMessage() {
-        var message = document.getElementById("subjectCodeMessage");
-        message.style.display = "none";
-    }
+function hideSubjectCodeMessage() {
+  var message = document.getElementById("subjectCodeMessage");
+  message.style.display = "none";
+}
 
-    function showSubjectMessage() {
-        var message = document.getElementById("subjectMessage");
-        message.style.display = "block";
-    }
+function showSubjectMessage() {
+  var message = document.getElementById("subjectMessage");
+  message.style.display = "block";
+}
 
-    function hideSubjectMessage() {
-        var message = document.getElementById("subjectMessage");
-        message.style.display = "none";
-    }
+function hideSubjectMessage() {
+  var message = document.getElementById("subjectMessage");
+  message.style.display = "none";
+}
+
+function showYearMessage() {
+  var message = document.getElementById("yearMessage");
+  message.style.display = "block";
+}
+
+function hideYearMessage() {
+  var message = document.getElementById("yearMessage");
+  message.style.display = "none";
+}
