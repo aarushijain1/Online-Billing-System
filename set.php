1b8b529b6a871b1d4a3f19b09ca99a2ebcf668c5
Gauri Agrawal <118062523+GauriGA@users.noreply.github.com>
2024-01-22 23:11:52 +0530

    Update set.php

--- set.php
+++ set.php
@@ -1,53 +1,50 @@
 <?php
-    session_start();
+session_start();
 
-    if (!isset($_SESSION['user'])) {
-        header('location: dashboard.php');
-        exit();
+if ( !isset( $_SESSION[ 'user' ] ) ) {
+    header( 'location: dashboard.php' );
+    exit();
+}
+
+include( 'connection.php' );
+
+// Get user details
+$user = $_SESSION[ 'user' ];
+
+if ( $_SERVER[ 'REQUEST_METHOD' ] === 'POST' ) {
+
+    $department = $_POST[ 'dept' ];
+    $off_email = $_POST[ 'off_email' ];
+    $program = $_POST[ 'prog' ];
+    $year = $_POST[ 'year' ];
+    $sem = $_POST[ 'sem' ];
+    $subjectCode = $_POST[ 'sub_code' ];
+    $subj = $_POST[ 'sub' ];
+    $charges = $_POST[ 'charges' ];
+    $expenses = $_POST[ 'expenses' ];
+    $total = $_POST[ 'total' ];
+    $currentDate = $_POST[ 'currentDate' ];
+
+    $query = "INSERT INTO erp_portal.paper_setting ( dept, off_email, prog, year, sem, sub_code, sub, charges, expenses, total, currentDate) 
+                  VALUES (:department, :off_email, :program, :year, :sem, :subjectCode, :subj, :charges, :expenses, :total, :currentDate)";
+    $stmt = $conn->prepare( $query );
+    $stmt->bindParam( ':department', $department );
+    $stmt->bindParam( ':off_email', $off_email );
+    $stmt->bindParam( ':program', $program );
+    $stmt->bindParam( ':year', $year );
+    $stmt->bindParam( ':sem', $sem );
+    $stmt->bindParam( ':subjectCode', $subjectCode );
+    $stmt->bindParam( ':subj', $subj );
+    $stmt->bindParam( ':charges', $charges );
+    $stmt->bindParam( ':expenses', $expenses );
+    $stmt->bindParam( ':total', $total );
+    $stmt->bindParam( ':currentDate', $currentDate );
+
+    if ( $stmt->execute() ) {
+        echo 'Form filled successfully!';
+    } else {
+        echo 'Error inserting data: ' . $stmt->errorInfo()[ 2 ];
     }
+}
 
-    include("connection.php");
-    
-    // Get user details
-    $user = $_SESSION['user'];
-    // $userId = $user['id'];                   // Assuming the user ID is stored in the 'id' field
-    
-
-    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-
-        // $formNo = $_POST['formNo'];
-        $department = $_POST['dept'];
-        $program = $_POST['prog'];
-        $sem = $_POST['sem'];
-        $subjectCode = $_POST['sub_code'];
-        $subj = $_POST['sub'];
-        $charges = $_POST['charges'];
-        $expenses = $_POST['expenses'];
-        $total = $_POST['total'];
-        
-        
-        $query = "INSERT INTO erp_portal.paper_setting ( dept, prog, sem, sub_code, sub, charges, expenses, total) 
-                  VALUES (:department, :program, :sem, :subjectCode, :subj, :charges, :expenses, :total)";
-        $stmt = $conn->prepare($query);
-        // $stmt->bindParam(':userId', $userId);
-        // $stmt->bindParam(':formNo', $formNo);
-        $stmt->bindParam(':department', $department);
-        $stmt->bindParam(':program', $program);
-        $stmt->bindParam(':sem', $sem);
-        $stmt->bindParam(':subjectCode', $subjectCode);
-        $stmt->bindParam(':subj', $subj);
-        $stmt->bindParam(':charges', $charges);
-        $stmt->bindParam(':expenses', $expenses);
-        $stmt->bindParam(':total', $total);
-    
-        if ($stmt->execute()) {
-            // Data inserted successfully
-            echo "Form filled successfully!";
-        } else {
-            // Handle the error
-            echo "Error inserting data: " . $stmt->errorInfo()[2];
-        }
-    }
-    
-    
 ?>
