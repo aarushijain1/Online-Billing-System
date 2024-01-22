a66254fda5a981e643889417040d11b397c2bff7
Gauri Agrawal <118062523+GauriGA@users.noreply.github.com>
2024-01-22 23:03:54 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,16 +1,15 @@
 <?php
-    $servername = 'localhost:3307';
-    $username = 'root';
-    $password = '';
-    $database = 'erp_portal';
-    $port = 3307;
+$servername = 'localhost:3307';
+$username = 'root';
+$password = '';
+$database = 'erp_portal';
+$port = 3307;
 
-    try {
-        $conn = new PDO("mysql:host=$servername; dbname=erp_portal", $username, $password);
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        // echo "connection successful";
-    } catch (\Exception $e) {
-        $error_message = $e->getMessage();
-    }
+try {
+    $conn = new PDO( "mysql:host=$servername; dbname=erp_portal", $username, $password );
+    $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+} catch ( \Exception $e ) {
+    $error_message = $e->getMessage();
+}
 
 ?>
