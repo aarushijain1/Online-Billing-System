cdaef2ab448888beab204d7ee322738bf09c0d33
Gauri Agrawal <118062523+GauriGA@users.noreply.github.com>
2024-01-22 23:04:26 +0530

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -1,45 +1,40 @@
 <?php
-    session_start();
-    
-    if(!isset($_SESSION['user'])) header('location: Login.php');
+session_start();
 
-    $user = $_SESSION['user'];
-   
+if ( !isset( $_SESSION[ 'user' ] ) ) header( 'location: Login.php' );
+
+$user = $_SESSION[ 'user' ];
+
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang = 'en'>
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <style> <?php include "dashboard.css" ?> </style>
-    <title>Dashboard</title>
-    <link rel="icon" href="logo.jpg" type="image/x-icon">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+<meta charset = 'UTF-8'>
+<meta name = 'viewport' content = 'width=device-width, initial-scale=1.0'>
+<style> <?php include 'dashboard.css' ?> </style>
+<title>Dashboard</title>
+<link rel = 'icon' href = 'logo.jpg' type = 'image/x-icon'>
+<link rel = 'stylesheet' href = 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css'>
 </head>
 
 <body>
 
-    <div class="dashboard">
-    <?php include('sidebar.php') ?>
-        <div class="main-content">
-            <?php include('navbar.php') ?>
-            <!-- Rest of your main content goes here -->
-        </div>
-        
-        <!-- sidebar -->
+<div class = 'dashboard'>
+<?php include( 'sidebar.php' ) ?>
+<div class = 'main-content'>
+<?php include( 'navbar.php' ) ?>
+</div>
 
-        <div class="main-content">
-            <!-- Rest of your main content goes here -->
-        </div>
-        
-    </div>
-    
+<div class = 'main-content'>
+</div>
 
+</div>
 
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/js/all.min.js"></script>
-    <script> <?php require_once("script.js");?> </script>
+<script src = 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/js/all.min.js'></script>
+<script> <?php require_once( 'script.js' );
+?> </script>
 
 </body>
 
