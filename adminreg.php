ab3760bd1023c3a23f1af0ca275164727cddb74a
Gauri Agrawal <118062523+GauriGA@users.noreply.github.com>
2024-01-22 23:02:43 +0530

    Update adminreg.php

--- adminreg.php
+++ adminreg.php
@@ -1,46 +1,44 @@
 <?php
 session_start();
 
-if ($_POST) {
-    include("connection.php");
+if ( $_POST ) {
+    include( 'connection.php' );
 
-    $first_name = $_POST['first_name'];
-    $last_name = $_POST['last_name'];
-    $dob = $_POST['dob'];
-    $gender = $_POST['gender'];
-    $mobileNumber = $_POST['mobileNumber'];
-    $offemail = $_POST['offemail'];
-    $address = $_POST['address'];
-    $country = $_POST['country'];
-    $state = $_POST['state'];
-    $city = $_POST['city'];
-    $district = $_POST['district'];
-    $pincode = $_POST['pincode'];
-    $image = $_POST['image'];
+    $first_name = $_POST[ 'first_name' ];
+    $last_name = $_POST[ 'last_name' ];
+    $dob = $_POST[ 'dob' ];
+    $gender = $_POST[ 'gender' ];
+    $mobileNumber = $_POST[ 'mobileNumber' ];
+    $offemail = $_POST[ 'offemail' ];
+    $address = $_POST[ 'address' ];
+    $country = $_POST[ 'country' ];
+    $state = $_POST[ 'state' ];
+    $city = $_POST[ 'city' ];
+    $district = $_POST[ 'district' ];
+    $pincode = $_POST[ 'pincode' ];
+    $image = $_POST[ 'image' ];
 
-    // Validate and sanitize user input to prevent SQL injection
-    $email = filter_var($email, FILTER_SANITIZE_EMAIL);
+    $email = filter_var( $email, FILTER_SANITIZE_EMAIL );
 
     $query = 'INSERT INTO erp_portal.adminnewreg (
         first_name, last_name, dob, gender, mobileNumber, offemail, address, country, state, city, district, pincode, image) 
         VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
-        $stmt = $conn->prepare($query);
-        $stmt->execute([
-        $first_name, $last_name, $dob, $gender, $mobileNumber, $offemail, $address, $country, $state, $city, $district, $pincode, 
-        $image]);
-    
+    $stmt = $conn->prepare( $query );
+    $stmt->execute( [
+        $first_name, $last_name, $dob, $gender, $mobileNumber, $offemail, $address, $country, $state, $city, $district, $pincode,
+        $image ] );
 
-    // Fetch the user information from the database after registration
-    // $query = 'SELECT * FROM users WHERE email = ?';
-    // $stmt = $conn->prepare($query);
-    // $stmt->execute([$email]);
+        // Fetch the user information from the database after registration
+        // $query = 'SELECT * FROM users WHERE email = ?';
+        // $stmt = $conn->prepare( $query );
+        // $stmt->execute( [ $email ] );
 
-    // if ($stmt->rowCount() > 0) {
-    //     $stmt->setFetchMode(PDO::FETCH_ASSOC);
-    //     $user = $stmt->fetchAll()[0];
-    //     $_SESSION['user'] = $user;
-    // }
+        // if ( $stmt->rowCount() > 0 ) {
+        //     $stmt->setFetchMode( PDO::FETCH_ASSOC );
+        //     $user = $stmt->fetchAll()[ 0 ];
+        //     $_SESSION[ 'user' ] = $user;
+        // }
 
-    header('Location: adminusername.php');
-}
-?>
+        header( 'Location: adminusername.php' );
+    }
+    ?>
